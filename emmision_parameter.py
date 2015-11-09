debb076260f4af0c74be530ec8a79526e704e43f
linyijuan <linyijuan22@gmail.com>
2015-11-09 15:41:16 +0800

    get_all_words function

--- emmision_parameter.py
+++ emmision_parameter.py
@@ -1,8 +1,18 @@
-def emmision_parameters(data_path):
+import numpy as np
+
+def get_all_words(filepaths):
+	x = []
+	for file in filepaths:
+		f_open = open(file, 'r')
+		total_line = sum(1 for line in open(file))
+		for i in range(total_line):
+			x.append(f_open.readline().strip())
+	return x
+
+def emmision_parameters(data_path, alphabet):
 	f_open = open(data_path, 'r')
 	total_line = sum(1 for line in open(data_path))
 	y = {}		# {tag: {word: num}}
-	e = []		# [tag][word]
 	for i in range(total_line):
 		line = f_open.readline().strip().split(' ')
 		if len(line) > 1:
@@ -14,20 +24,23 @@ def emmision_parameters(data_path):
 					y[line[1]][line[0]] = 1
 				else:
 					y[line[1]][line[0]] += 1
-	# e = []
-	# for key in y:
-	# 	# print y[key]
-	# 	for sub_key in y[key]:
-	# 		# print y[key][sub_key]
-	# 		tmp = y[key][sub_key]/
-	# 		e.append()
-
-
-
+	
+	#fix probability for all new words
+	e = []		# [tag][word]
+	for key in y:
+#		print y[key]
+		for sub_key in y[key]:
+#			print y[key][sub_key]
+			if (y[key][sub_key] in alphabet):
+				e[key] = {sub_key:y[key][sub_key]}
+				
+			tmp = y[key][sub_key]/
+			e.append()
 
 
 def main():
-	emmision_parameters('/Users/abc/Desktop/POS/train')
+	emmision_parameters('/Users/abc/Desktop/POS
+	_dataset/train',get_all_words('/Users/abc/Desktop/POS_dataset/dev.in'))
 
 
 
